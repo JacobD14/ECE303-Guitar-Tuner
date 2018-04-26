2e4fb662689d7e5a2c4867abd8ac0c4a503a215b
Jacob Decker <32854146+JacobD14@users.noreply.github.com>
2018-04-25 22:06:42 -0400

    Continuously reads in the voltage

--- Guitar_Frequency.ino
+++ Guitar_Frequency.ino
@@ -1,9 +1,10 @@
-int energy = 0;
+unsigned long energy;
 int voltread = 0;
 int count = 0;
 int period = 0;
 int frequency = 0;
-
+unsigned long t3;
+unsigned long t4;
 void setup() {
   Serial.begin(9600);
 }
@@ -14,14 +15,19 @@ void loop() {
   // Start the timer
   unsigned long t1 = millis();
 
-  for (int i = 0; i < 20; i++) {
-    voltread = analogRead(A0);
+  energy = 0;
+
+  for (int i = 0; i < 4500; i++) {
+    voltread = analogRead(A1);
+
     energy = energy + sq(voltread-512);
   }
 
   // End the timer
   unsigned long t2 = millis();
-  
+
+  Serial.print("Voltage is: ");
+  Serial.println(voltread);
   Serial.print("Energy: ");
   Serial.println(energy);
   Serial.print("Time taken (in milliseconds): ");
@@ -29,38 +35,38 @@ void loop() {
 
 // --------------------------
 
-// ---------- Finding Frequency ---------
+// ---------- Finding Frequency --------- // Continously check for voltage!!!
 
-  if (voltread > 600) {
+  if (analogRead(A1) > 600) {
     
   while (count < 20) {
     // While voltage is above zero crossing
-    while (voltread > 512); // Wait
+    while (analogRead(A1) > 512); // Wait
 
     // Start timer
-    unsigned long t3 = micros();
+    t3 = micros();
     
     delayMicroseconds(625);
 
     // Wait for voltage to cross back
-    while (voltread < 512); // Wait
+    while (analogRead(A1) < 512); // Wait
 
     count++;
 
     delayMicroseconds(625);
 
-    while (voltread > 512);
+    while (analogRead(A1) > 512);
 
     count++;
   }
   
-  unsigned long t4 = micros();
+    t4 = micros();
   
   }
 
   period = ((t4 - t3) / 10) * 1000000;
 
-  frequency = 1 / period;
+  frequency = (1/period);
 
   Serial.print("The frequency is: ");
   Serial.println(frequency);
