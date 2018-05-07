60819972589e7ae8444a2856da03ba4e3c1b0279
Jacob Decker <32854146+JacobD14@users.noreply.github.com>
2018-05-07 13:38:18 -0400

    Updated and added more comments

--- Guitar_Frequency.ino
+++ Guitar_Frequency.ino
@@ -1,17 +1,14 @@
 // Jacob Decker and Nick Allen
-// Miami University 
+// Miami University
 
 // This code is used to find the frequency of the particular
-// guitar string plucked. It first takes samples to find the energy, 
+// guitar string plucked. It first takes samples to find the energy,
 // then analyzes the voltage wave to find the period, and then the frequency.
 
 unsigned long energy;
-int voltread = 0;
-int count = 0;
-int period = 0;
-int frequency = 0;
-unsigned long t3;
-unsigned long t4;
+unsigned long voltread = 0;
+unsigned long count = 0;
+
 
 void setup() {
   Serial.begin(9600);
@@ -19,7 +16,9 @@ void setup() {
 
 void loop() {
 
-// ---------- Samples ---------
+  // First find the total energy of the plucked string
+
+  // ---------- Samples ---------
   // Start the timer
   unsigned long t1 = millis();
 
@@ -29,60 +28,92 @@ void loop() {
   for (int i = 0; i < 4500; i++) {
     voltread = analogRead(A1);
 
-    energy = energy + sq(voltread-512);
+    energy = energy + sq(voltread - 460);
   }
 
   // End the timer
   unsigned long t2 = millis();
 
-  Serial.print("Voltage is: ");
-  Serial.println(voltread);
-  Serial.print("Energy: ");
-  Serial.println(energy);
-  Serial.print("Time taken (in milliseconds): ");
-  Serial.println(t2 - t1);
+  //  Serial.print("Voltage is: ");
+  //  Serial.println(voltread);
+  //  Serial.print("Energy: ");
+  // Serial.println(energy);
+  //  Serial.print("Time taken (in milliseconds): ");
+  //  Serial.println(t2 - t1);
 
-// --------------------------
+  // --------------------------
 
-// ---------- Finding Frequency --------- 
 
-  if (analogRead(A1) > 600) {
-    
-  while (count < 20) {
-    // While voltage is above zero crossing
-    while (analogRead(A1) > 512); // Wait
+  // ---------- Finding Frequency ---------
+  // This part only starts if the energy is above 25,000,000 and if
+  // the voltage being read in is greater than 480.
+  // This counts the amount of times the wave crosses the zero line.
+  // Once it crosses 20 times, it breaks out of the loop.
+  // Using the time it took to go through that, we can calculate period,
+  // and then the frequency.
 
-    // Records time right after it crosses
-    t3 = micros();
-    
-    delayMicroseconds(625);
+  unsigned long t3;
+  unsigned long t4;
 
-    // Wait for voltage to cross back
-    while (analogRead(A1) < 512); // Wait
+  if (energy > 25000000) {
 
-    count++;
+    Serial.println("Getting Frequency");
 
-    delayMicroseconds(625);
+    if (analogRead(A1) > 480) {
+      delay(1000);
+      count = 0;
+      while (count < 20) {
+        // While voltage is above zero crossing
+        while (analogRead(A1) - 6 > 460); // Wait
 
-    // Wait for another cross, completing the period
-    while (analogRead(A1) > 512);
+        // Records time right after it crosses
+        t3 = micros();
 
-    count++;
+        delayMicroseconds(1000);
+
+        // Wait for voltage to cross back
+        while (analogRead(A1) - 6 < 460); // Wait
+
+        count++;
+
+        delayMicroseconds(1000);
+
+        // Wait for another cross, completing the period
+        while (analogRead(A1) - 6 > 460);
+
+        count++;
+      }
+
+      // Records the time after the 20 crosses
+      t4 = micros();
+
+    }
   }
 
-    // Records the time after the 20 crosses
-    t4 = micros();
-  
-  }
+  // Reset count for next loop through
+  count = 0;
 
-  // Calculate the period 
-  period = ((t4 - t3) / 10) * 1000000;
+  // Time taken in microseconds
+  float timeSec = t4 - t3;
 
-  frequency = (1/period);
+  // Converting from microseconds to seconds
+  float timeSec2 = timeSec / 100000;
+
+  // Calculate the period
+  float period = timeSec2 / 10;
+
+  // Calculate the frequency
+  float frequency = 1 / period;
 
   Serial.print("The frequency is: ");
   Serial.println(frequency);
-  
-// -----------------------------------
+  Serial.println("=============");
+
+  delay(600);
+
+  // -----------------------------------
+
 }
+
+
 
